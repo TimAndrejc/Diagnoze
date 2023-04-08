402d47e3a99b3cfff9722bfa6dd56205f666e6dd
Tim <tim.andrejc02@gmail.com>
2023-04-08 18:36:18 +0200

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post '/Diagnosis', to: 'diagnosis#new'
   # Defines the root path route ("/")
   root to: "symptoms#index" 
   # Defines the route for the symptoms controller
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
   # Defines the route for the bodyparts controller
   resources :bodyparts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/Diagnosis", to: "diagnosis#new"
+  
 end
